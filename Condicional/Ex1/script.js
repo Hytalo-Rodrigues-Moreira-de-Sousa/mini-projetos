b35e37b582813e1a5eab633266334b4605df0dd9
Hytalo Rodrigues Moreira de Sousa <hytalo.rmds.trabalho@gmail.com>
2022-11-02 17:26:07 -0300

    1

--- Condicional/Ex1/script.js
+++ Condicional/Ex1/script.js
@@ -1,22 +1,16 @@
 function verificar(){
-    let a = document.getElementById('txta')
-    let b = document.getElementById('txtb')
-    let c = document.getElementById('txtc')
-
-    if(a.value.length == 0 && b.value.length == 0 && c.value.length == 0){
-        window.alert('Digite um valor para prosseguir')
-    }
-    else{
-        res.innerHTML = 'Verificando...: '
-        let n1 = Number(a.value)
-        let n2 = Number(b.value)
-        let n3 = Number(c.value)
-    }
-    res.innerHTML = ''
+    let a = document.getElementById('na');
+    let b = document.getElementById('nb');
+    let c = document.getElementById('nc');
+    let res = document.getElementById('res');
+    
     if(a + b < c){
-        res.innerHTML += (`A soma de ${n1} + ${n2} é maior que ${n3}`)
+        res.innerHTML = `A soma de ${a} + ${b} é menor que ${c}`;
+    }
+    else if(a + b > c){
+        res.innerHTML = `A soma de ${a} + ${b} não é menor que ${c}`;
     }
     else{
-        res.innerHTML += (`A soma de ${n1} + ${n2} não é maior que ${n3}`)
+        res.innerHTML = `A soma de ${a} + ${b} é igual a ${c}`;
     }
 }
